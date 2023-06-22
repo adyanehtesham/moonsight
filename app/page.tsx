773a858c9917e586a0e7697c1ccfd22727fb0aa8
Adyan <67388808+adyanehtesham@users.noreply.github.com>
2023-06-21 23:28:32 -0400

    display prayer timing

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,93 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+type Prayer = {
+  day: string
+  times: {
+    fajr: string
+    sunrise: string
+    dhuhr: string
+    asr: string
+    maghrib: string
+    isha: string
+  }
+} | null
 
 export default function Home() {
+
+  const [prayers, setPrayers] = useState({
+    fajr: '',
+    sunrise: '',
+    dhuhr: '',
+    asr: '',
+    maghrib: '',
+    isha: '',
+  })
+  const [location, setLocation] = useState({ latitude: 0, longitude: 0 })
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+
+
+    const today = new Date()
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+    window.navigator.geolocation.getCurrentPosition(position => {
+      setLocation(position.coords)
+    })
+
+    axios.get(`https://www.moonsighting.com/time_json.php?year=${today.getFullYear()}&tz=${timezone}&lat=${location.latitude}&lon=${location.longitude}&method=0&both=false&time=0`)
+      .then(prayerResponse => {
+        const todayString = `${today.toLocaleString('default', { month: 'short', day: '2-digit' })} ${today.toLocaleDateString('default', { weekday: 'short' })}`
+
+        const prayersToday = prayerResponse.data.times.find((prayer: Prayer) => prayer?.day === todayString)
+
+        setPrayers(prayersToday.times)
+
+        if (prayers.fajr !== '') {
+          setLoading(false)
+        }
+
+      })
+      .catch(error => { console.log(error) })
+
+  }, [setPrayers, setLocation, location.latitude, location.longitude, prayers.fajr])
+
+  if (loading) {
+    return <>
+      <main className={styles.main}>
+        <h1 className={styles.heading}>Moonsight</h1>
+        <p className={styles.loading}>Loading...</p>
+      </main>
+    </>
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h1 className={styles.heading}>Moonsight</h1>
+      <ul className={styles.prayers}>
+        <li>
+          <p>Fajr</p><p>{prayers.fajr}</p>
+        </li>
+        <li>
+          <p>Sunrise</p><p>{prayers.sunrise}</p>
+        </li>
+        <li>
+          <p>Dhuhr</p><p>{prayers.dhuhr}</p>
+        </li>
+        <li>
+          <p>Asr</p><p>{prayers.asr}</p>
+        </li>
+        <li>
+          <p>Maghrib</p><p>{prayers.maghrib}</p>
+        </li>
+        <li>
+          <p>Isha</p><p>{prayers.isha}</p>
+        </li>
+      </ul>
     </main>
   )
 }
